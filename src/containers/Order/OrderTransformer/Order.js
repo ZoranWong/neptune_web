93fe73bb3a661448e1c6edeaa3282171bd63fafe
MeasponeS <958183117@qq.com>
2020-03-05 16:56:07 +0800

    uploadExcal

---
+++ src/containers/Order/OrderTransformer/Order.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom'
+
+class OrderTransformer extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+		
+		};
+	}
+	
+	componentWillMount() {
+	
+	}
+	
+	
+	
+	render(){
+		
+		
+		return (
+			<div className="orderTransformer">
+			
+			</div>
+		)
+	}
+}
+export default withRouter(OrderTransformer)
