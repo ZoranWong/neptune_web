97ab6c34cd1ec23cb9c4f40e75471d0ee25b555f
Q <42404280+MeasponeS@users.noreply.github.com>
2020-04-03 18:02:08 +0800

    准备写活动回显

---
+++ src/containers/Activities/AllActivities/modal/PreviewDetails.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PreviewDetails extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PreviewDetails;
