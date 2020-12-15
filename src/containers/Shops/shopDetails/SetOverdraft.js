868321ccccc0961508933475698da91971592ed1
wx15056046046 <wx15056046046@163.com>
2020-12-15 16:59:16 +0800

    fix

--- src/containers/Shops/shopDetails/SetOverdraft.js
+++ src/containers/Shops/shopDetails/SetOverdraft.js
@@ -1,90 +1,23 @@
-// import React, {Component} from 'react';
-// import {Input, message, Modal} from "antd";
-// import {setOverdraft} from "../../../api/shops/shopManage";
-
-// class SetOverdraft extends Component {
-// 	state = {
-// 		overdraft: ''
-// 	};
-	
-// 	componentWillReceiveProps(nextProps, nextContext) {
-// 		if (!nextProps.overdraft) return ;
-// 		this.setState({overdraft: nextProps.overdraft})
-// 	}
-	
-// 	handleCancel = () => {
-// 		this.props.onCancel();
-// 	};
-	
-// 	handleSubmit = () => {
-// 		let id = this.props.id;
-// 		if (!this.state.overdraft || this.state.balanceValue < 0) {
-// 			message.error('透支额度不合法');
-// 			return
-// 		}
-// 		setOverdraft({
-// 			shop_id: id,
-// 			overdraft: this.state.overdraft
-// 		}).then(r=>{
-// 			message.success(r.message);
-// 			this.handleCancel();
-// 			this.props.refresh();
-// 			this.setState({overdraft: 0})
-// 		}).catch(_=>{})
-// 	};
-	
-// 	render() {
-// 		return (
-// 			<div>
-// 				<Modal
-// 					title='透支额度调整'
-// 					width={520}
-// 					visible={this.props.visible}
-// 					onCancel={this.handleCancel}
-// 					onOk={this.handleSubmit}
-// 					okText="确定"
-// 					cancelText="取消"
-// 				>
-// 					<div className="operateBalance">
-// 						<span>设置透支额度</span>
-// 						<Input
-// 							value={this.state.overdraft}
-// 							type='number'
-// 							onChange={(e)=>{
-// 								if (e.target.value < 0) {
-// 									message.error('透支额度不可小于0');
-// 									return;
-// 								}
-// 								this.setState({overdraft:e.target.value})
-// 							}}
-// 						/>
-// 					</div>
-// 				</Modal>
-// 			</div>
-// 		);
-// 	}
-// }
-
-// export default SetOverdraft;
 import React, {Component} from 'react';
 import './css/index.sass'
 import {Input, message, Modal, Radio,Button} from "antd";
-import {adjustShopBalance,sendVerificationCode,verificationCode} from "../../../api/shops/shopManage";
+import {setOverdraft,sendVerificationCode,verificationCode} from "../../../api/shops/shopManage";
 // import {adjustUserBalance} from "../../../api/user";
 var timer =null
 class SetOverdraft extends Component {
 	state = {
 		value: 'add',
-		remark: '',
 		userPhone:15056046046,
 		countDown:60,
-		password:''
+		password:'',
+		overdraft:'',
+		// captcha:''
 	};
 	componentWillReceiveProps(nextProps, nextContext) {
 		console.log(nextProps,'nextPropsnextPropsnextProps')
-		if (!nextProps.overdraft) return ;
+		// if (!nextProps.overdraft) return ;
 
-		this.setState({overdraft: nextProps.overdraft})
+		// this.setState({overdraft: nextProps.overdraft})
 	}
 
 	handleCancel = () => {
@@ -111,21 +44,19 @@ class SetOverdraft extends Component {
 		// let field = this.props.type === 'user'? 'user_id' : 'shop_id';
 		let id = this.props.id;
 		console.log(id,"=============id============")
-		if (!this.state.balanceValue || this.state.balanceValue < 0) {
+		if (!this.state.overdraft || this.state.overdraft < 0) {
 			message.error('余额值不合法');
 			return
 		}
-		adjustShopBalance({
-			amount: this.state.balanceValue,
-			operation: this.state.value,
+		setOverdraft({
+			overdraft: this.state.overdraft,
 			shop_id: id,
-			remark: this.state.remark,
-		 	captcha:this.state.password
+			captcha:this.state.password
 		}).then(r=>{
 			message.success(r.message);
 			this.handleCancel();
 			this.props.refresh();
-			this.setState({balanceValue: 0, remark: ''})
+			this.setState({overdraft: 0,})
 		}).catch(_=>{})
 	};
 
@@ -150,9 +81,6 @@ class SetOverdraft extends Component {
 						<Input
 							disabled
 							value={this.state.userPhone}
-							// onChange={(e)=>{
-							// 	this.setState({remark:e.target.value})
-							// }}
 						/>
 						
 					</div>
@@ -185,23 +113,14 @@ class SetOverdraft extends Component {
 							<Radio value='sub'>减少</Radio>
 						</Radio.Group>
 						<Input
-							value={this.state.balanceValue}
+							value={this.state.overdraft}
 							type='number'
 							onChange={(e)=>{
 								if (e.target.value < 0) {
 									message.error('余额变动不可小于0');
 									return;
 								}
-								this.setState({balanceValue:e.target.value})
-							}}
-						/>
-					</div>
-					<div className="operateBalanceRemark">
-						<span>备注:</span>
-						<Input
-							value={this.state.remark}
-							onChange={(e)=>{
-								this.setState({remark:e.target.value})
+								this.setState({overdraft:e.target.value})
 							}}
 						/>
 					</div>
